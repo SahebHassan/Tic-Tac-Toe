131c3fa7183418e0c62a4fca93a0e666a2b399f6
SahebHassan <124240923+SahebHassan@users.noreply.github.com>
2023-08-03 21:43:19 +0530

    added function to javascript

---
+++ scripts.js
@@ -0,0 +1,16 @@
+let music = new Audio("music.mp3")
+let turn = new Audio("ting.mp3")
+let gameover = new Audio("gameover.mp3")
+let turn = "X";
+
+//fuction to change the turn
+const changeTurn =()=>{
+    return turn ==="X" ? "0": "X"
+}
+
+//Function to check foe win
+const checkWin =()=>{
+
+}
+
+// Game Logic
